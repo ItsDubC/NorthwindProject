4f663c6fcb7f034cde15b5521071544fbc142ff7
ItsDubC <itsdubc@gmail.com>
2015-07-13 17:08:56 -0500

    Initial checkin

---
+++ NorthwindProject.Entities/Concrete/Category.cs
@@ -0,0 +1,21 @@
+﻿using NorthwindProject.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace NorthwindProject.Entities.Concrete
+{
+    public class Category : IEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public List<Product> Products { get; set; }
+
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+    }
+}
