12dba52df5c302cbde20ac010540980c9d9c0e13
ItsDubC <itsdubc@gmail.com>
2015-07-16 21:13:37 -0500

    Started business layer

---
+++ NorthwindProject.Business/Abstract/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NorthwindProject.Entities.Concrete;
+using NorthwindProject.Entities.ComplexTypes;
+
+
+namespace NorthwindProject.Business.Abstract
+{
+    public interface IProductService
+    {
+        List<Product> GetAll(ProductFilter filter);
+        Product GetById(int productId);
+        List<Product> GetByCategoryId(int categoryId);
+        void Add(Product product);
+        void Update(Product product);
+        void Delete(Product product);
+        void DeleteById(int productId);
+    }
+}
---
+++ NorthwindProject.Business/Concrete/Managers/ProductManager.cs
@@ -0,0 +1,47 @@
+﻿using NorthwindProject.Business.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthwindProject.Business.Concrete.Managers
+{
+    public class ProductManager : IProductService
+    {
+        public List<Entities.Concrete.Product> GetAll(Entities.ComplexTypes.ProductFilter filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Entities.Concrete.Product GetById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Entities.Concrete.Product> GetByCategoryId(int categoryId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Add(Entities.Concrete.Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Entities.Concrete.Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(Entities.Concrete.Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
